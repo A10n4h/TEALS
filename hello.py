ff1566b2860cf79fbd296ead4c096e1ebf2bae70
A10n4h <113188213+A10n4h@users.noreply.github.com>
2022-11-16 08:38:34 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,3 @@
 print("hello world from my TEALS binder")
-[![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/A10n4h/TEALS.git/HEAD)
+[![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/A10n4h/HEAD
 Markdown icon
